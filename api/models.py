e1a033608d1c3b9bf9929ab3e58aa0ef57df79ae
frankyle <magaifrank@gmail.com>
2024-10-20 19:19:42 +0300

    Am trying to solve registration error

--- api/models.py
+++ api/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, Group, Permission
+from django.core.exceptions import ObjectDoesNotExist
 
 class User(AbstractUser):
     username = models.CharField(max_length=100)
@@ -23,8 +24,12 @@ class User(AbstractUser):
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
 
+    @property
     def profile(self):
-        profile = Profile.objects.get(user=self)
+        try:
+            return self.profile_set.get()  # Use the reverse relationship to get the profile
+        except ObjectDoesNotExist:
+            return None
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -33,13 +38,12 @@ class Profile(models.Model):
     image = models.ImageField(upload_to="user_images", default="default.jpg")
     verified = models.BooleanField(default=False)
 
+# Signal to create or update user profile
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
-
-def save_user_profile(sender, instance, **kwargs):
-    instance.profile.save()
+    else:
+        instance.profile.save()
 
 from django.db.models.signals import post_save
 post_save.connect(create_user_profile, sender=User)
-post_save.connect(save_user_profile, sender=User)
